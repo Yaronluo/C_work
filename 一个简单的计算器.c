490b4050f9db1895555d01177275d95b28334f4f
yaron-luo <2575586108@qq.com>
2025-10-17 23:21:22 +0800

    C语言学习

--- 一个简单的计算器.c
+++ 一个简单的计算器.c
@@ -1,35 +1,44 @@
 #include <stdio.h>
-         int main()
-         {
-             char sign;
-             double firstnumber, secondnumber;
-             printf("请输入计算符号(+,-,/,*)");
-             scanf("%c",&sign);
-             printf("请输入第一个数字和第二个数字");
-             int numeber = scanf("%lf%lf",&firstnumber,&secondnumber);
-             printf("输入数字个数为%d\n",numeber);
-             if (secondnumber == 0 &&sign == '/')
-             {
-             printf("计算失败零不可以作除数");//为了防止0除任何数字程序错误
-             }
-             else
-             {
-                 switch (sign)//符号不同所采取的计算方式也不同
-                 {
-                 case '+':
-                     printf("%lf+%lf=%lf\n",firstnumber,secondnumber,firstnumber+secondnumber);
-                     break;
-                 case '-':
-                     printf("%lf-%lf=%lf\n",firstnumber,secondnumber,firstnumber-secondnumber);
-                     break;
-                 case '*':
-                     printf("%lf*%lf=%lf\n",firstnumber,secondnumber,firstnumber*secondnumber);
-                     break;
-                 case '/':
-                     printf("%lf/%lf=%lf\n",firstnumber,secondnumber,firstnumber/secondnumber);
-                     break;
-
-                 }
-             }
-
-         return 0;}
+int main()
+{
+    do
+    {
+        char sign;
+        double firstnumber, secondnumber;
+        printf("------请输入计算符号(+,-,/,*)------\n");
+        printf("  ------输入q可以退出计算器------\n");
+        scanf(" %c",&sign);//%c前面要加空格，清空或跳过输入缓冲区中的空白字符
+        if (sign == 'q')
+        {
+            printf("程序已经退出");
+            break;
+        }
+        printf("请输入第一个数字和第二个数字");
+        scanf("%lf%lf",&firstnumber,&secondnumber);
+        if (secondnumber == 0 &&sign == '/')
+        {
+            printf("计算失败零不可以作除数");//为了防止0除任何数字程序错误
+        }
+        else
+        {
+            switch (sign)//符号不同所采取的计算方式也不同
+            {
+            case '+':
+                printf("%.2lf+%.2lf=%.2lf\n",firstnumber,secondnumber,firstnumber+secondnumber);
+                break;
+            case '-':
+                printf("%.2lf-%.2lf=%.2lf\n",firstnumber,secondnumber,firstnumber-secondnumber);
+                break;
+            case '*':
+                printf("%.2lf*%.2lf=%.2lf\n",firstnumber,secondnumber,firstnumber*secondnumber);
+                break;
+            case '/':
+                printf("%.2lf/%.2lf=%.2lf\n",firstnumber,secondnumber,firstnumber/secondnumber);
+                break;
+            default:
+                printf("  =====请输入正确的计算符号=====\n\n");
+            }
+        }
+    }while(1);
+    return 0;
+}
---
+++ 猜数字游戏.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void game ()//game函数引用
+{
+     printf("注意你只有十次机会哦!!!\n");
+     int number = rand() % 100 + 1;//产生随机数，不能放在while循环内，否则会不断随机
+     for (int times = 1; times <= 10; times++)
+     {
+         printf("---第%d次---\n",times);
+         int input = 0;
+         printf("请输入一个数字(1~100)\n");
+         scanf("%d",&input);
+         if (input > 100 || input <= 0)
+         {
+             printf("请输入正确的数字\n");
+             continue;
+         }
+         if (input > number)
+         {
+             printf("---猜大了---\n\n");
+         }
+         else if (input < number)
+         {
+             printf("---猜小了---\n\n");
+         }
+         else
+         {
+             printf("恭喜你猜对了!!!\n\n");
+             break;
+         }
+         if (times == 10 && number != input)//次数用尽显示的画面
+         {
+             printf(" 《===次数已用尽===》\n");
+             printf("《===正确答案是%d===》\n",number);
+             printf(" 《===再玩一次吧===》\n\n");
+         }
+     }
+}
+int main()
+{
+    srand((unsigned int)time(NULL));//使种子随机不至于导致每次生成的随机数相同，不需要放在循环中，不然会一直改变种子，没必要！而且会有小bug
+    int sign = 0;
+    printf("欢迎游玩猜数字游戏\n");
+    do
+    {
+      printf("------------------------\n");
+      printf("------开始游戏请输入1------\n");
+      printf("------退出游戏请输入0------\n");
+      printf("------------------------\n");
+      scanf("%d",&sign);
+      switch (sign)
+      {
+          case 1:
+          game();//引用函数game
+          break;
+          case 0:
+          printf("欢迎下次游玩");
+          break;
+          default:
+          printf("请输入正确的数字!!!");
+          break;
+      }
+    }
+    while (sign);
+    return 0;
+}
