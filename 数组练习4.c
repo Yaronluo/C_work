19022b3de4e5ac01229da65a762e354cb850b5f0
yaron-luo <2575586108@qq.com>
2025-10-23 00:05:53 +0800

    C语言学习

---
+++ 数组练习4.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int left = 0;
+    //int right = strlen(arr)-1;
+    int right = sizeof(arr)/sizeof(arr[0])-1;
+    int number = 0;
+    printf("请输入1~10的数字");
+    do
+    {
+        scanf("%d",&number);
+        if (number > 10||number<1)
+        {
+            printf("请输入正确的数字");
+        }
+        else
+        {
+            break;
+        }
+    }while(1);
+    while (left <= right)
+    {
+        int mid = (left + right) / 2;
+        if (arr[mid] == number)
+        {
+            printf("这个数字的下标是%d",mid);
+            break;
+        }
+        else if (arr[mid] < number)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return 0;
+}
